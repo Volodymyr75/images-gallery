e14d0dc4477b4b2a0be2787cc719c84aa5101e14
Volodymyr Strembovskyi <strembov@gmail.com>
2022-05-22 14:52:47 +0300

    controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+
+}
 
 const Header = ({ title }) => {
     // = (props) => ... const { title } = props; ..use {title}
     // = (props) => ... use {props.title}
   return (
-    <Navbar bg="dark" variant="light">
-        <Navbar.Brand href="/"> {title} </Navbar.Brand>
+    <Navbar style={navbarStyle} variant="light">
+        <Container>
+            <Navbar.Brand href="/"> {title} </Navbar.Brand>
+        </Container>
+        
     </Navbar>
   );
 };
