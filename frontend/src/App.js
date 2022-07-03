adee48a33360da21c1c5d9e2a911e41f9be2ad3d
Volodymyr Strembovskyi <strembov@gmail.com>
2022-07-03 11:57:34 +0300

    spinner in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import Search from './components/Search';
 import ImageCard from './components/ImageCard';
 import Welcome from './components/Welcome';
+import Spinner from './components/Spinner';
 import { Container, Row, Col } from 'react-bootstrap';
 
 // const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
@@ -14,6 +15,7 @@ const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5050';
 const App = () => {
   const [word, setWord] = useState('');
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // console.log(images);
   // const getSavedImages = async () => {
@@ -32,6 +34,7 @@ const App = () => {
       try {
         const res = await axios.get(`${API_URL}/images`);
         setImages(res.data || []);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -111,26 +114,37 @@ const App = () => {
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
-      {/* {!!images.length && <ImageCard image={images[0]} />} */}
-      <Container className="mt-4">
-        {images.length ? (
-          <Row xs={1} md={2} lg={3}>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Search
+            word={word}
+            setWord={setWord}
+            handleSubmit={handleSearchSubmit}
+          />{' '}
+          {/* {!!images.length && <ImageCard image={images[0]} />} */}{' '}
+          <Container className="mt-4">
             {' '}
-            {images.map((image, i) => (
-              <Col key={i} className="pb-3">
-                <ImageCard
-                  image={image}
-                  deleteImage={handleDeleteImage}
-                  saveImage={handleSaveImage}
-                />{' '}
-              </Col>
-            ))}{' '}
-          </Row>
-        ) : (
-          <Welcome />
-        )}
-      </Container>
+            {images.length ? (
+              <Row xs={1} md={2} lg={3}>
+                {' '}
+                {images.map((image, i) => (
+                  <Col key={i} className="pb-3">
+                    <ImageCard
+                      image={image}
+                      deleteImage={handleDeleteImage}
+                      saveImage={handleSaveImage}
+                    />{' '}
+                  </Col>
+                ))}{' '}
+              </Row>
+            ) : (
+              <Welcome />
+            )}{' '}
+          </Container>
+        </>
+      )}
     </div>
   );
 };
